e0c28f78675939fc94468d7c9f63d3dde7e7163a
EliottJaquierCPNV <61775618+EliottJaquierCPNV@users.noreply.github.com>
2023-03-06 11:20:10 +0100

    Fixed mover tests

--- Assets/Tests/MoverTest.cs
+++ Assets/Tests/MoverTest.cs
@@ -32,13 +32,13 @@ public class MoverTest
     public IEnumerator Mover_MoveUp_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(1, 0, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(1, 0, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = 0;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -46,13 +46,13 @@ public class MoverTest
     public IEnumerator Mover_MoveDown_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(-1, 0, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(-1, 0, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = 180;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -60,13 +60,13 @@ public class MoverTest
     public IEnumerator Mover_MoveLeft_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(0, -1, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(0, -1, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = -90;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -74,13 +74,13 @@ public class MoverTest
     public IEnumerator Mover_MoveRight_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(0, 1, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(0, 1, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = 90;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -88,13 +88,13 @@ public class MoverTest
     public IEnumerator Mover_MoveUpRight_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(0.7f, 0.7f, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(0.7f, 0.7f, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = 45;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -102,13 +102,13 @@ public class MoverTest
     public IEnumerator Mover_MoveDownLeft_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(-0.7f, -0.7f, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(-0.7f, -0.7f, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 1;
         mover.Direction = 225;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -118,12 +118,12 @@ public class MoverTest
     public IEnumerator Mover_MoveSlow_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(0.5f, 0, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(0.5f, 0, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 0.5f;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -131,12 +131,12 @@ public class MoverTest
     public IEnumerator Mover_MoveFast_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(5f, 0, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(5f, 0, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Speed = 5f;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -144,13 +144,13 @@ public class MoverTest
     public IEnumerator Mover_MoveSlowDiagonal_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(0.4f,0.4f, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(0.355f,0.355f, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Direction = 45;
         mover.Speed = 0.5f;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition =GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -158,13 +158,13 @@ public class MoverTest
     public IEnumerator Mover_MoveFastDiagonal_PositionEquals()
     {
         //Given
-        Vector3 expectedPosition = new Vector3(3.5f,3.5f, 0);
+        Vector3 expectedPosition = GetApproximatePosition(new Vector3(3.55f,3.55f, 0) * TEST_WAIT_TIME);
         Mover mover = SpawnMoverPlayer();
         mover.Direction = 45;
         mover.Speed = 5f;
         //When
         yield return new WaitForSecondsRealtime(TEST_WAIT_TIME);//Testing update of Mover
-        Vector3 actualPosition = GetApproximatePosition(mover.transform);
+        Vector3 actualPosition = GetApproximatePosition(mover.transform.position);
         //Then
         Assert.AreEqual(expectedPosition, actualPosition);
     }
@@ -176,8 +176,8 @@ public class MoverTest
         Mover mover = gm.AddComponent<Mover>();
         return mover;
     }
-    Vector3 GetApproximatePosition(Transform moverTransform)
+    Vector3 GetApproximatePosition(Vector3 position)
     {
-        return new Vector3((float)Math.Round(moverTransform.position.x, DIGITS_POSITION_TOLERANCE), (float)Math.Round(moverTransform.position.y, DIGITS_POSITION_TOLERANCE), 0);
+        return new Vector3((float)Math.Round(position.x, DIGITS_POSITION_TOLERANCE), (float)Math.Round(position.y, DIGITS_POSITION_TOLERANCE), 0);
     }
 }
