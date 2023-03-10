d3aeb3ba5599be6ebb695b60e4f082843c62ac4b
EliottJaquierCPNV <61775618+EliottJaquierCPNV@users.noreply.github.com>
2023-03-10 10:30:47 +0100

    Documented the robots to better understand the internal workings (even the private methods)

--- Assets/Scripts/Players/Bots/BotPlayer.cs
+++ Assets/Scripts/Players/Bots/BotPlayer.cs
@@ -19,12 +19,49 @@ public class BotPlayer : Player
     float _velocity;
     Vector2 _lastCachedBasePosition;
 
+    void Update()
+    {
+        if (IsNeedingANewTarget())
+        {
+            ChooseNewTarget();
+            return;
+        }
+
+        //The target position is not necesarry it's transform position (ex : a base has a closer valid point) 
+        Vector3 targetWorldPosition = (_target is Base) ? _lastCachedBasePosition : _target.transform.position;
+        
+        Move(targetWorldPosition);
+        CheckTargetAccomplishement(targetWorldPosition);
+        TryToPickup();
+    }
+
+    /// <summary>
+    /// Is the current target expired or not valid ? Do we need to choose an other ?
+    /// </summary>
+    /// <returns>True if the target need to be changed</returns>
+    bool IsNeedingANewTarget()
+    {
+        if (_target == null) return true;
+
+        //The target pickup object has a owner
+        if (_target is PickupObject && ((PickupObject)_target).Owner != _pickupController) return true;
+
+        //The flower target has no more pollen
+        if (_target is Flower && ((Flower)_target).HasPollen() == false) return true;
+
+        return false;
+    }
+
+    /// <summary>
+    /// Choose a random new target
+    /// </summary>
     void ChooseNewTarget()
     {
         float randomPercentage = Random.Range(0, 1f);
         if (randomPercentage < PASSIVITY)
         {
             _target = GetNearObject<Flower>();
+            //If no compatible flowers found, go to single pollen units
             if (_target == null) _target = GetNearObject<Pollen>();
         }
         else
@@ -33,36 +70,32 @@ public class BotPlayer : Player
         }
     }
 
-    void Update()
-    {
-        if (_target == null || (_target is PickupObject && ((PickupObject)_target).Owner != _pickupController))
-        {
-            ChooseNewTarget();
-            return;
-        }
-        
-        Vector3 targetWorldPosition = (_target is Base) ? _lastCachedBasePosition : _target.transform.position;
-        
-        Move(targetWorldPosition);
-        CheckTargetAccomplishement(targetWorldPosition);
-        TryToPickup();
-    }
-
+    /// <summary>
+    /// Change direction to match the target direction
+    /// </summary>
+    /// <param name="targetWorldPosition">The target to move to</param>
     void Move(Vector3 targetWorldPosition)
     {
         Vector3 targetRelativeDirection = targetWorldPosition-transform.position;
+
         //Convert the target direction relative vector to an angle
         float angle = Mathf.Atan2(targetRelativeDirection.y, targetRelativeDirection.x) * Mathf.Rad2Deg;
+
         //The direction is smoothed
         float smothAngle = Mathf.SmoothDampAngle(_mover.Direction, angle, ref _velocity, SMOOTH_DIRECTION * Time.deltaTime);
         _mover.Direction = smothAngle;
     }
-    
+
+    /// <summary>
+    /// Check if the target is reached
+    /// </summary>
+    /// <param name="targetWorldPosition">The target real world position</param>
     void CheckTargetAccomplishement(Vector3 targetWorldPosition)
     {
         float distanceFromTarget = Vector3.Distance(transform.position, targetWorldPosition);
         if (distanceFromTarget < DROP_DISTANCE_TOLERANCE)
         {
+            //Drop current objects if target is a base
             if (_target is Base && _pickupController.GetPickedUpObjects().Count != 0)
             {
                 _pickupController.Drop();
@@ -71,6 +104,9 @@ public class BotPlayer : Player
         }
     }
     
+    /// <summary>
+    /// Try to pickup any objects compatible on the current position
+    /// </summary>
     void TryToPickup()
     {
         PickupObject pickup = _pickupController.GetCompatiblePickableObject();
@@ -78,15 +114,19 @@ public class BotPlayer : Player
         {
             _pickupController.PickupLastObject();
 
+            //Calculate the risk to choose what to do next
             float randomPercentage = Random.Range(0, 1f);
             if (randomPercentage > RISK)
             {
+                //Go to a base depending on the type of object
                 if (pickup is Pollen) _target = _base;
                 if (pickup is Pesticide) _target = GetNearestOtherBase();
+                //Using cached positions to not compute the NearestValidPlacablePosition at every frames
                 _lastCachedBasePosition = ((Base)_target).GetNearestValidPlacablePosition(transform.position);
             }
             else
             {
+                //Go to search more similar objects
                 if (pickup is Pollen)
                     _target = GetNearObject<Pollen>();
                 if (pickup is Pesticide)
@@ -95,6 +135,10 @@ public class BotPlayer : Player
         }
     }
 
+    /// <summary>
+    /// Get the nearest base of an other player
+    /// </summary>
+    /// <returns>The nearest base found</returns>
     Base GetNearestOtherBase()
     {
         float minDistance = Mathf.Infinity;
@@ -113,12 +157,21 @@ public class BotPlayer : Player
         }
         return nearestBase;
     }
-    
+
+    /// <summary>
+    /// Get a object of a given type that is near (not necessary the nearest to randomise the behavior)
+    /// </summary>
+    /// <typeparam name="T">The type of object targetted</typeparam>
+    /// <returns>The object found or null if no matches</returns>
     PlacableObject GetNearObject<T>() where T : PlacableObject
     {
+        //Get the list of objects by the distance
         List<PlacableObject> objectsByDistance = GameManager.Instance.ObjectsManager.GetSpawnedObjectsByType<T>().OrderBy(point => Vector3.Distance(transform.position, point.transform.position)).ToList();
+        
+        //Start to search the object with a NEAR_OBJECT_ERROR index
         for (int i = Mathf.Min(objectsByDistance.Count, NEAR_OBJECT_ERROR) - 1; i < objectsByDistance.Count; i++)
         {
+            //Determine if the object is compatible (no owner and interest)
             PlacableObject objectToTest = objectsByDistance[i];
             if (objectToTest is PickupObject && ((PickupObject)objectToTest).Owner != null) continue;
             if (objectToTest is Flower && (((Flower)objectToTest).HasPollen() == false)) continue;
