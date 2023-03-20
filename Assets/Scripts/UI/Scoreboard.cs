952c2e2f1d9eb9e36c45470e5db23227aa3fe3c3
EliottJaquierCPNV <61775618+EliottJaquierCPNV@users.noreply.github.com>
2023-03-20 15:11:11 +0100

    Optimised allocations in scoreboard

--- Assets/Scripts/UI/Scoreboard.cs
+++ Assets/Scripts/UI/Scoreboard.cs
@@ -31,23 +31,16 @@ public class Scoreboard : MonoBehaviour
         if (!_needUpdate) return;
         _needUpdate = false;
 
-        //Get scores
-        List<(string, int)> scores = new List<(string, int)>();
-        foreach (var player in GameManager.Instance.Players.Players)
-        {
-            scores.Add((player.Base.Name, player.Base.Score));
-        }
-
-        //Order scores
-        List<(string, int)> scoresOrdered = scores.OrderByDescending(score => score.Item2).ToList();
+        //Order players by score
+        List<Player> playersOredered = GameManager.Instance.Players.Players.OrderByDescending(player => player.Base.Score).ToList();
 
         //Display scores
         for (int i = 0; i < _names.Length; i++)
         {
-            if (i < scoresOrdered.Count)
+            if (i < playersOredered.Count)
             {
-                _names[i].text = scoresOrdered[i].Item1;
-                _scores[i].text = scoresOrdered[i].Item2 + " pts";
+                _names[i].text = playersOredered[i].Base.Name;
+                _scores[i].text = playersOredered[i].Base.Score + " pts";
             }
             else
             {
