9294a434f3e176d12fb00387b1b7d04ac548a182
EliottJaquierCPNV <61775618+EliottJaquierCPNV@users.noreply.github.com>
2023-05-08 16:40:47 +0200

    BEESIO-54 : Fixed small issued with server (better random distribution + test compilation regression)

--- Server/src/commonStructures/random.ts
+++ Server/src/commonStructures/random.ts
@@ -1,16 +1,14 @@
-import { randomInt } from "crypto";
-
 /**
  * Random number generator. Used as an equivalent to the Random class in C# Unity.
  */
 export default class Random {
     /**
-     * Generates a random number between 0 and 1.
+     * Generates a random number between min and max.
      * @param min The minimum value of the random number.
      * @param max The maximum value of the random number.
      * @returns The random number.
      */
     public static Range(min:number,max:number):number{
-        return randomInt(max-min)+min;
+        return (Math.random()*(max-min))+min;
     }
 }
